b2bf3a4acf046b74716df93c929cb9164fd9e50f
Jovan Nikolic <jovannikolic@gmail.com>
2022-02-01 11:58:16 +0100

    Zadatak1 bez provere

---
+++ src/d31_01_2022/zadatak1.java
@@ -0,0 +1,36 @@
+package d31_01_2022;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class zadatak1 {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "driver-lib/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		
+		driver.navigate().to("https://s.bootsnipp.com/iframe/Dq2X");
+		driver.manage().window().maximize();
+		List<WebElement>links=driver.findElements(By.xpath("//*[@type='button']"));
+		List<WebElement>baneri=driver.findElements(By.xpath("//*[contains(@class,'alert-')]"));
+		boolean daLiSeUgasilo;
+		int brojac=0;
+	
+		
+		for (int i=links.size()-1;i>=0;i--) {
+	
+		driver.findElements(By.xpath("(//*[@type='button'])[last()]"));
+			links.get(i).click();
+	
+			
+		}
+	} 
+}
+
--- src/domaci28_01_2022/domaci1.java
+++ src/domaci28_01_2022/domaci1.java
@@ -2,6 +2,11 @@ package domaci28_01_2022;
 import java.util.List;
 import java.util.Scanner;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 
 public class domaci1 {
 
